88076720b6bc5053b3659ed6f4479ab7cbd0a29a
Todd Sukolsky <tsukolsky@sigovs.com>
2014-08-27 22:06:38 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <cstdlib.h>
+#include <stdio.h>
+#include <ctime>
+
+#define TEST
+
+int main()
+{
+	while(1)
+	{
+		sleep(1);
+	}
+	return 0;
+}
---
+++ xbee.h
@@ -0,0 +1,5 @@
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#define TEST
